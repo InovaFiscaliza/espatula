6c3b929b4937065bf64a9cd7c3a37c0dc17036e7
ronaldokun <ronaldokun@gmail.com>
2024-07-01 16:42:12 +0900

    ✨ feat: Enhance product data extraction and add web scraping library
    - Added the `gazpacho` library for web scraping
    - Renamed the `extract_product_data` method to `extract_search_data` to better reflect its purpose
    - Implemented a new `parse_tables` method to extract product details from HTML tables
    - Expanded the `extract_item_data` method to capture more product data points, including category, rating, review count, brand, model, certification, and EAN
    - Refactored and optimized the `extract_item_data` method for better readability and maintainability

--- magalu.py
+++ magalu.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from dataclasses import dataclass
 
 import typer
+from gazpacho import Soup
 from base import BaseScraper, KEYWORDS
 
 
@@ -13,7 +14,7 @@ class MagaluScraper(BaseScraper):
     input_field: str = 'input[data-testid="input-search"]'
     next_page_button: str = 'button[aria-label="Go to next page"]'
 
-    def extract_product_data(self, produto):
+    def extract_search_data(self, produto):
         relative_url = produto.attrs.get("href")
         if name := produto.find(
             "h2", attrs={"data-testid": "product-title"}, mode="first"
@@ -56,10 +57,100 @@ class MagaluScraper(BaseScraper):
             partial=True,
             mode="all",
         ):
-            if product_data := self.extract_product_data(item):
+            if product_data := self.extract_search_data(item):
                 product_data["Palavra_Chave"] = keyword
                 results[product_data["Link"]] = product_data
         return results
+
+    def parse_tables(self, soup) -> dict:
+        # Extrai o conteúdo da tabela com dados do produto e transforma em um dict
+        variant_data = {}
+        if conteiner := soup.find(
+            "div", attrs={"class": "sc-fqkvVR dESRav sc-czLspv jSiKdc"}, mode="first"
+        ):
+            for table in conteiner.find("table", mode="all"):
+                if rows := table.find("td", mode="all"):
+                    if rows[0].strip() == "Informações complementares":
+                        continue
+                    variant_data.update(
+                        {
+                            k.strip(): v.strip()
+                            for k, v in zip(rows[::2], rows[1::2])
+                            if ("R$" not in k.strip() and "R$" not in v.strip())
+                        }
+                    )
+        return variant_data
+
+    def extract_item_data(self, driver):
+        soup = Soup(driver.get_page_source())
+
+        if categoria := soup.find(
+            "a", attrs={"data-testid": "breadcrumb-item"}, mode="all", partial=False
+        ):
+            driver.highlight("a[data-testid=breadcrumb-item]")
+            categoria = " | ".join(
+                i.strip() for i in categoria if hasattr(i, "strip") and i.strip()
+            )
+
+        if nome := soup.find(
+            "h1", attrs={"data-testid": "heading-product-title"}, mode="first"
+        ):
+            driver.highlight("h1[data-testid=heading-product-title]")
+            nome = nome.text.strip()
+
+        nota, avaliações = None, None
+        if popularidade := soup.find(
+            "div", attrs={"data-testid": "mod-row"}, mode="first"
+        ):
+            driver.highlight("div[data-testid=mod-row]")
+            if popularidade := popularidade.find(
+                "span", attrs={"format": "score-count"}, mode="first"
+            ):
+                nota, avaliações = popularidade.text.strip().split(" ")
+                avaliações = avaliações.replace("(", "").replace(")", "")
+
+        if preço := soup.find("p", attrs={"data-testid": "price-value"}, mode="first"):
+            driver.highlight("p[data-testid=price-value]")
+            preço = (
+                preço.text.strip().replace("R$", "").replace(".", "").replace(",", ".")
+            )
+
+        if imgs := soup.find("img", {"data-testid": "media-gallery-image"}, mode="all"):
+            driver.highlight("img[data-testid=media-gallery-image]")
+            imgs = [getattr(i, "attrs", {}).get("src") for i in imgs]
+
+        if descrição := soup.find(
+            "div", attrs={"data-testid": "rich-content-container"}, mode="first"
+        ):
+            driver.highlight("div[data-testid=rich-content-container]")
+            descrição = descrição.text.strip()
+
+        marca, modelo, certificado, ean = None, None, None, None
+        if características := self.parse_tables(soup):
+            driver.highlight("div[class=sc-fqkvVR dESRav sc-czLspv jSiKdc]")
+            marca, modelo, certificado, ean = (
+                características.get("Marca"),
+                características.get("Modelo"),
+                self.extrair_certificado(características),
+                características.get("EAN"),
+            )
+
+        if not all([nome, categoria, preço, imgs]):
+            return None
+        return {
+            "Nome": nome,
+            "Categoria": categoria,
+            "Preço": preço,
+            "Nota": nota,
+            "Avaliações": avaliações,
+            "Imagem": imgs,
+            "Descrição": descrição,
+            "Marca": marca,
+            "Modelo": modelo,
+            "Certificado": certificado,
+            "EAN": ean,
+            "Data_Atualização": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+        }
 
 
 if __name__ == "__main__":
@@ -71,6 +162,7 @@ if __name__ == "__main__":
         md: bool = False,
     ):
         scraper = MagaluScraper(headless=headless)
+
         if not keyword:
             for keyword in KEYWORDS:
                 scraper.search(keyword, screenshot, md)
