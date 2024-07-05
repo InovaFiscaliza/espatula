f7d13b2348e8c291d85c670f0d0f34f2ace4f224
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-07-05 14:06:57 -0300

    ✨ feat: Refine product information extraction in MercadoLivreScraper
    - Simplified the logic for extracting the "estado" and "vendas" information from the product page
    - Removed unnecessary conditional checks and improved the robustness of the data extraction process

--- mercado_livre.py
+++ mercado_livre.py
@@ -158,13 +158,13 @@ class MercadoLivreScraper(BaseScraper):
         ):
             self.highlight_element(driver, "span[class=ui-pdp-subtitle]")
             if len(info_vendas := info_vendas.strip().split(" | ")) == 2:
-                estado, vendas = info_vendas.strip().split(" | ")
-            elif len(info_vendas) == 1:
-                vendas = info_vendas.strip()
-                if estado := soup.find(
-                    "p", attrs={"class": "andes-badge__content"}, mode="first"
-                ):
-                    estado = estado.strip()
+                estado, vendas = info_vendas
+            # elif len(info_vendas) == 1:
+            #     vendas = info_vendas[0].strip()
+            #     if estado := soup.find(
+            #         "p", attrs={"class": "andes-badge__content"}, mode="first"
+            #     ):
+            #         estado = estado.strip()
 
         if nome := soup.find("h1", attrs={"class": "ui-pdp-title"}, mode="first"):
             self.highlight_element(driver, "h1[class=ui-pdp-title]")
