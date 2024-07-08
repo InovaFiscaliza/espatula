11cb10ca4e5e96097de6e8685747b5f10975837a
ronaldokun <ronaldokun@gmail.com>
2024-07-09 00:48:43 +0900

    ✨ feat: Implement product data extraction in AmericanasScraper
    - Added support for extracting product data from the Americanas website
    - Implemented methods to parse the HTML content and extract relevant information such as product name, category, price, brand, model, certification, EAN, and product characteristics
    - Utilized the `gazpacho` library for HTML parsing and extraction
    - Highlighted relevant HTML elements using the `highlight_element` method for debugging purposes
    - Refactored the `parse_tables` method to extract product characteristics from HTML tables

--- americanas.py
+++ americanas.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from dataclasses import dataclass
+from gazpacho import Soup
 
 from base import BaseScraper, TIMEZONE
 
@@ -57,3 +58,94 @@ class AmericanasScraper(BaseScraper):
                 product_data["palavra_busca"] = keyword
                 results[product_data["url"]] = product_data
         return results
+
+    def extract_item_data(self, driver):
+        soup = Soup(driver.get_page_source())
+
+        if categoria := soup.find(
+            "div", attrs={"data-testid": "breadcrumb"}, mode="first", partial=False
+        ):
+            self.highlight_element(driver, "div:contains(breadcrumb)")
+            categoria = " | ".join(
+                i.strip()
+                for i in categoria.find("a", mode="all")
+                if hasattr(i, "strip") and i.strip()
+            )
+
+        if nome := soup.find("h1", attrs={"class": "product-title"}, mode="first"):
+            self.highlight_element(driver, 'h1:contains("product-title")')
+            nome = nome.strip()
+
+        # if imagens := soup.find("div", {"class": "Gallery"}, mode="first"):
+        #     self.highlight_element(driver, 'div:contains("Gallery")')
+        #     imagens = [
+        #         getattr(i, "attrs", {}).get("src")
+        #         for i in imagens.find("img", mode="all")
+        #     ]
+
+        # nota, avaliações = None, None
+        # if popularidade := soup.find(
+        #     "div", attrs={"data-testid": "mod-row"}, mode="first"
+        # ):
+        #     self.highlight_element(driver, "div[data-testid=mod-row]")
+        #     if popularidade := popularidade.find(
+        #         "span", attrs={"format": "score-count"}, mode="first"
+        #     ):
+        #         nota, avaliações = popularidade.text.strip().split(" ")
+        #         avaliações = avaliações.replace("(", "").replace(")", "")
+
+        if preço := soup.find("div", attrs={"class": "priceSales"}, mode="first"):
+            # self.highlight_element(driver, "div[data-testid=mod-productprice]")
+            preço.text.strip().replace("R$", "").replace(".", "").replace(",", ".")
+
+        else:
+            preço = None
+
+        # if descrição := soup.find(
+        #     "div", attrs={"data-testid": "rich-content-container"}, mode="first"
+        # ):
+        #     self.highlight_element(driver, "div[data-testid=rich-content-container]")
+        #     descrição = descrição.text.strip()
+
+        marca, modelo, certificado, ean, product_id = None, None, None, None, None
+        if características := self.parse_tables(soup):
+            marca, modelo, certificado, ean = (
+                características.get("Marca"),
+                características.get("Modelo"),
+                self.extrair_certificado(características),
+                características.get("EAN"),
+                características.get("Código"),
+            )
+
+        return {
+            "nome": nome,
+            "categoria": categoria,
+            "preço": preço,
+            # "nota": nota,
+            # "avaliações": avaliações,
+            # "imagens": imagens,
+            # "descrição": descrição,
+            "marca": marca,
+            "modelo": modelo,
+            "certificado": certificado,
+            "ean_gtin": ean,
+            "características": características,
+            "product_id": product_id,
+            "data": datetime.now().astimezone(TIMEZONE).strftime("%Y-%m-%dT%H:%M:%S"),
+        }
+
+    def parse_tables(self, soup) -> dict:
+        # Extrai o conteúdo da tabela com dados do produto e transforma em um dict
+        variant_data = {}
+        for table in soup.find("table", mode="all"):
+            if rows := table.find("td", mode="all"):
+                if rows[0].strip() == "Informações complementares":
+                    continue
+                variant_data.update(
+                    {
+                        k.strip(): v.strip()
+                        for k, v in zip(rows[::2], rows[1::2])
+                        if ("R$" not in k.strip() and "R$" not in v.strip())
+                    }
+                )
+        return variant_data
