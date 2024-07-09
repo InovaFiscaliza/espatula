8132a80987f9f29b36cf9b35b63595003c7df96d
ronaldokun <ronaldokun@gmail.com>
2024-07-09 12:44:10 +0900

    ✨ feat: Parallelize website scraping tasks
    - Changed the `Start-Process` commands to run the scraping tasks in parallel by adding the `&` character at the end of each command
    - This allows the script to scrape data from multiple websites concurrently, improving the overall performance of the data collection process

--- inspect_pages.ps1
+++ inspect_pages.ps1
@@ -3,8 +3,8 @@ $PYTHON_EXE = "D:\Applications\Scoop\apps\miniconda3\current\envs\sb\python.exe"
 $SCRIPT_PATH = ".\scripts\scrape.py"
 
 Set-Location $WORKING_DIR
-Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH mercado_livre --keyword smartphone --no-search --screenshot" -WindowStyle Hidden
-Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH amazon --keyword smartphone --no-search --screenshot" -WindowStyle Hidden
-Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH magalu --keyword smartphone --no-search --screenshot" -WindowStyle Hidden
-Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH carrefour --keyword smartphone --no-search --screenshot" -WindowStyle Hidden
-Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH americanas --keyword smartphone --no-search --screenshot" -WindowStyle Hidden
+Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH mercado_livre --keyword smartphone --no-search --screenshot" &
+Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH amazon --keyword smartphone --no-search --screenshot" &
+Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH magalu --keyword smartphone --no-search --screenshot" &
+Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH carrefour --keyword smartphone --no-search --screenshot" & 
+Start-Process -FilePath $PYTHON_EXE -ArgumentList "$SCRIPT_PATH americanas --keyword smartphone --no-search --screenshot" & 
