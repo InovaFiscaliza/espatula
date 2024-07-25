1f4fd972275cc050b267855599bcd9f31b02ba4e
ronaldokun <ronaldokun@gmail.com>
2024-07-25 22:59:22 +0900

    🎨 Removed CONFORMIDADE registers from computing sch data and simplified returned cols to only a subset of interest.

--- espatula/certificacao.py
+++ espatula/certificacao.py
@@ -12,59 +12,62 @@ from espatula.constantes import FOLDER
 load_dotenv(find_dotenv(), override=True)
 
 CERTIFICADO_COLUMNS = {
-    "Data da Homologação": "Data_da_Homologação",
+    "Data da Homologação": "Data_de_Homologação",
     "Número de Homologação": "certificado",
     "Nome do Solicitante": "Nome_do_Solicitante",
     "Data de Validade do Certificado": "Data_de_Validade_do_Certificado",
     "Situação do Certificado": "Situação_do_Certificado",
-    "Nome do Fabricante": "fabricante",
-    "Modelo": "Modelo",
-    "Nome Comercial": "Nome_Comercial",
-    "Tipo do Produto": "Tipo_do_Produto",
+    "Nome do Fabricante": "fabricante_sch",
+    "Modelo": "modelo_sch",
+    "Nome Comercial": "nome_sch",
+    "Tipo do Produto": "tipo_sch",
 }
 
-CONFORMIDADE_COLUMNS = {
-    "NumeroHomologacao": "certificado",
-    "DataEmissaoHomologacao": "Data_de_Homologação",
-    "Fabricante": "fabricante",
-    "Produto": "Tipo_do_Produto",
-    "Modelo": "Modelo",
-    "NomeComercial": "Nome_Comercial",
-    "StatusRequerimento": "Situação_do_Certificado",
-}
+# CONFORMIDADE_COLUMNS = {
+#     "NumeroHomologacao": "certificado",
+#     "DataEmissaoHomologacao": "Data_de_Homologação",
+#     "Fabricante": "fabricante",
+#     "Produto": "tipo_sch",
+#     "Modelo": "modelo",
+#     "NomeComercial": "nome",
+#     "StatusRequerimento": "Situação_do_Certificado",
+# }
 
 G5_COLUMNS = {
     "NumeroHomologacao": "certificado",
     "DataEmissaoHomologacao": "Data_de_Homologação",
-    "TipodeProduto": "Tipo_do_Produto",
-    "Modelo": "Modelo",
-    "NomeComercial": "Nome_Comercial",
-    "Fabricante": "fabricante",
+    "TipodeProduto": "tipo_sch",
+    "Modelo": "modelo_sch",
+    "NomeComercial": "nome_sch",
+    "Fabricante": "fabricante_sch",
 }
 
-MARCA_ANATEL_COLUMNS = {
-    "Tipo de Produto": "Tipo_do_Produto",
-    "Modelo do produto": "Modelo",
-    "Nome Comercial": "Nome_Comercial",
-    "Empresa Requerente": "Nome_do_Solicitante",
-    "Fabricante": "fabricante",
-    "Status da Homologação": "Situação_do_Certificado",
-    "Data": "Data_de_Homologação",
-}
+# MARCA_ANATEL_COLUMNS = {
+#     "Tipo de Produto": "tipo_sch",
+#     "Modelo do produto": "modelo",
+#     "Nome Comercial": "nome",
+#     "Empresa Requerente": "Nome_do_Solicitante",
+#     "Fabricante": "fabricante",
+#     "Status da Homologação": "Situação_do_Certificado",
+#     "Data": "Data_de_Homologação",
+# }
 
-MATCH_COLUMNS = {
-    "Nome_Comercial": "nome_sch",
-    "fabricante_x": "fabricante",
-    "fabricante_y": "fabricante_sch",
-    "Modelo_x": "modelo",
-    "Modelo_y": "modelo_SCH",
-}
+# MATCH_COLUMNS = {
+#     "nome_x": "nome",
+#     "nome_y": "nome_sch",
+#     "fabricante_x": "fabricante",
+#     "fabricante_y": "fabricante_sch",
+#     "modelo_x": "modelo",
+#     "modelo_y": "modelo_sch",
+# }
+
+COLS_SCH = ["nome_sch", "fabricante_sch", "modelo_sch", "tipo_sch"]
 
 
 def update_sch():
     for source in [
         os.environ["CERTIFICADOS"],
-        os.environ["CONFORMIDADE"],
+        # os.environ["CONFORMIDADE"],
         os.environ["G5"],
     ]:
         urllib.request.urlretrieve(
@@ -86,9 +89,9 @@ def read_sch(update: bool = False) -> pd.DataFrame:
         update_sch()
 
     certificados_path = FOLDER / "produtos_certificados.zip"
-    conformidade_path = (
-        FOLDER / "Produtos_Homologados_por_Declaração_de_Conformidade.zip"
-    )
+    # conformidade_path = (
+    #     FOLDER / "Produtos_Homologados_por_Declaração_de_Conformidade.zip"
+    # )
     celulares_path = FOLDER / "celulares_5g_homologados.zip"
 
     if not certificados_path.is_file():
@@ -101,15 +104,15 @@ def read_sch(update: bool = False) -> pd.DataFrame:
     )
     certificados.rename(columns=CERTIFICADO_COLUMNS, inplace=True)
 
-    if not conformidade_path.is_file():
-        update_sch()
-    conformidade = pd.read_csv(
-        conformidade_path,
-        dtype="string",
-        sep=";",
-        usecols=CONFORMIDADE_COLUMNS.keys(),
-    )
-    conformidade.rename(columns=CONFORMIDADE_COLUMNS, inplace=True)
+    # if not conformidade_path.is_file():
+    #     update_sch()
+    # conformidade = pd.read_csv(
+    #     conformidade_path,
+    #     dtype="string",
+    #     sep=";",
+    #     usecols=CONFORMIDADE_COLUMNS.keys(),
+    # )
+    # conformidade.rename(columns=CONFORMIDADE_COLUMNS, inplace=True)
 
     if not celulares_path.is_file():
         update_sch()
@@ -123,11 +126,15 @@ def read_sch(update: bool = False) -> pd.DataFrame:
 
     celulares = celulares[~celulares.certificado.isin(certificados.certificado)]
 
-    return pd.concat([certificados, conformidade, celulares], ignore_index=True)
+    df = pd.concat([certificados, celulares], ignore_index=True)
+
+    cols = ["certificado"] + COLS_SCH
+
+    return df.loc[:, cols]
 
 
 def merge_to_sch(
-    df: pd.DataFrame, update: bool = False
+    df: pd.DataFrame, update: bool = False, tipo_sch: str = None
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Merges the given DataFrame df with the SCH homologation database table.
 
@@ -160,7 +167,6 @@ def merge_to_sch(
         indicator=True,
     ).astype("string")
 
-    hm.drop_duplicates(inplace=True)
-    hm = hm.rename(columns=MATCH_COLUMNS)
-    columns = df.columns.tolist() + list(MATCH_COLUMNS.values())
-    return hm.loc[:, columns]
+    if tipo_sch is not None:
+        hm = hm.loc[hm.tipo_sch == tipo_sch].drop_duplicates().reset_index(drop=True)
+    return hm
