38f36079019623f62817e6400c8302eeb8cb7446
ronaldokun <ronaldokun@gmail.com>
2024-09-03 17:10:09 +0900

    Removed data from processing.
    Now all comes from API call

--- espatula/spiders/base.py
+++ espatula/spiders/base.py
@@ -19,13 +19,22 @@ from seleniumbase.common.exceptions import (
     NoSuchElementException,
 )
 
-from espatula.constantes import (
-    CERTIFICADO,
-)
 
 load_dotenv(find_dotenv(), override=False)
 TIMEZONE = ZoneInfo("America/Sao_Paulo")
 TODAY = datetime.today().astimezone(TIMEZONE).strftime("%Y%m%d")
+CERTIFICADO = re.compile(
+    r"""
+    (?ix)                  # Case-insensitive and verbose mode
+    ^                      # Start of the string
+    (Anatel[:\s]*)?        # Optional "Anatel" followed by colon or spaces
+    (                      # Start of main capturing group
+        (\d[-\s]*)+        # One or more digits, each optionally followed by hyphen or spaces
+    )
+    $                      # End of the string
+""",
+    re.VERBOSE,
+)
 
 
 @dataclass
