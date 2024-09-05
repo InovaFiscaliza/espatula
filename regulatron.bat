2a783d84e8143a637e859e82de32004b122c56b3
ronaldokun <ronaldokun@gmail.com>
2024-09-05 14:34:01 +0900

    feat: Copy config.json to app directory in regulatron.bat
    This commit updates the regulatron.bat script to copy the config.json file from the root directory to the app directory. This ensures that the necessary configuration file is available in the correct location when running the Espatula application.

--- regulatron.bat
+++ regulatron.bat
@@ -2,6 +2,7 @@
 if not exist "%USERPROFILE%\.cargo\bin\uv.exe" (
     powershell -c "irm https://astral.sh/uv/0.4.4/install.ps1 | iex"
 )
+copy .\config.json .\app\config.json
 cd .\app
 call %USERPROFILE%\.cargo\bin\uv.exe run run.py
 start "" "http://localhost:8502"
