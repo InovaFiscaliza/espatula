b585265acf32488564673b4d22cd09e2a818d816
ronaldokun <ronaldokun@gmail.com>
2024-09-05 18:40:34 +0900

    fix: Update regulatron.bat script
    This commit updates the regulatron.bat script with the following changes:
    
    1. Copies the config.json file to the app directory.
    2. Runs the uv.exe command with the sync argument to ensure dependencies are up-to-date.
    3. Starts the uv.exe run command to execute the run.py file.
    4. Adds a 5-second timeout before opening the application in the default browser at http://localhost:8501.
    
    These changes improve the reliability and consistency of the application startup process.

--- regulatron.bat
+++ regulatron.bat
@@ -4,5 +4,7 @@ if not exist "%USERPROFILE%\.cargo\bin\uv.exe" (
 )
 copy .\config.json .\app\config.json
 cd .\app
-call %USERPROFILE%\.cargo\bin\uv.exe run run.py
-start "" "http://localhost:8502"
+call %USERPROFILE%\.cargo\bin\uv.exe sync
+start "" %USERPROFILE%\.cargo\bin\uv.exe run run.py
+timeout /t 5 /nobreak > nul
+start "" "http://localhost:8501"
