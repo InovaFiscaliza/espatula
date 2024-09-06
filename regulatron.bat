8049c54b9bd12b7e9e540366ab22af95b80a4a36
ronaldokun <ronaldokun@gmail.com>
2024-09-06 14:50:15 +0900

    fix: Update regulatron.bat script
    This commit updates the regulatron.bat script with the following changes:
    
    1. Sets the ESPATULA_BIN environment variable to the appropriate directory.
    2. Creates the ESPATULA directory if it doesn't exist.
    3. Sets the CARGO_DIST_FORCE_INSTALL_DIR environment variable to the ESPATULA directory.
    4. Enables the RemoteSigned execution policy for the current user.
    5. Installs the uv.exe binary if it doesn't exist in the ESPATULA\bin directory.
    6. Copies the config.json file to the app directory.
    7. Runs the uv.exe command with the sync argument to ensure dependencies are up-to-date.
    8. Starts the uv.exe run command to execute the run.py file.
    9. Adds a 5-second timeout before opening the application in the default browser at http://localhost:8501.
    
    These changes improve the reliability and consistency of the application startup process.

--- regulatron.bat
+++ regulatron.bat
@@ -1,10 +1,23 @@
 @echo off
-if not exist "%USERPROFILE%\.cargo\bin\uv.exe" (
-    powershell -c "irm https://astral.sh/uv/0.4.4/install.ps1 | iex"
+setx ESPATULA_BIN "%APPDATA%\Anatel\SCH\bin"
+setlocal
+
+set "ESPATULA=%APPDATA%\Anatel\SCH"
+
+if not exist "%ESPATULA%" mkdir "%ESPATULA%"
+set CARGO_DIST_FORCE_INSTALL_DIR=%ESPATULA%
+
+powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+
+if not exist "%ESPATULA%\bin\uv.exe" (
+    powershell -Command "irm https://astral.sh/uv/install.ps1 | iex"
 )
+
+endlocal
+
 copy .\config.json .\app\config.json
 cd .\app
-call %USERPROFILE%\.cargo\bin\uv.exe sync
-start "" %USERPROFILE%\.cargo\bin\uv.exe run run.py
+call "%ESPATULA_BIN%\uv.exe" sync
+start "" "%ESPATULA_BIN%\uv.exe" run run.py
 timeout /t 5 /nobreak > nul
 start "" "http://localhost:8501"
