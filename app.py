21a81ec2514a59858fbdd3a45fbd0ea8165971f1
ronaldokun <ronaldokun@gmail.com>
2024-09-10 23:58:48 +0900

    ✨ feat: Simplify cache-related state management in app.py
    - Use a more concise assignment for `st.session_state.use_cache`
    - Remove unnecessary variable assignment for `use_cache`

--- app.py
+++ app.py
@@ -86,8 +86,7 @@ if "cache" not in st.session_state:
     st.session_state.cache = {}
 
 if "use_cache" not in st.session_state:
-    use_cache = CACHE[0] if CONFIG.get(CACHE[0]) else CACHE[1]
-    st.session_state.use_cache = use_cache
+    st.session_state.use_cache = CACHE[0] if CONFIG.get(CACHE[0]) else CACHE[1]
 
 if "show_cache" not in st.session_state:
     st.session_state.show_cache = False
