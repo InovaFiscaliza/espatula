e59e56673fefbb8f9d6f331ca4b9b9410c6851d7
ronaldokun <ronaldokun@gmail.com>
2024-09-27 19:28:29 +0900

    cleanup: remove commented code from release script
    - Cleared out unused and outdated lines to improve code readability and maintainability

--- release.ps1
+++ release.ps1
@@ -62,29 +62,6 @@ $destinationFolder = "D:\OneDrive - ANATEL\Regulatron"
 
 Copy-FilesToDestination -filesToCopy $filesToCopy -destinationFolder $destinationFolder
  
-# Get-ChildItem -Path $destinationFolder -Recurse | ForEach-Object {
-#     $_.Attributes = $_.Attributes -bor [System.IO.FileAttributes]::ReadOnly
-# }
-# cd $destinationFolder
-
-# cd ..
-
-# 7z a -sfx Regulatron.sfx Regulatron
-
-# copy /b Regulatron.sfx + 7z.config.txt Regulatron.exe
 
 Write-Host "Created app folder with the required contents"
-
-# $filesToZip = @(
-#     "app",
-#     "Regulatron.bat"
-
-# )
-
-# Compress-Archive -Path $filesToZip -DestinationPath "D:\OneDrive - ANATEL\AppRegulatron.zip" -Force
-
-# Write-Host "Created release Regulatron.zip file with required contents"
-
-
-# Remove-Item -Path "app" -Recurse -Force -ErrorAction SilentlyContinue
 
